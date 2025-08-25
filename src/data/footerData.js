d1facd8bad090cd7dc7a466961b10bcc2126b630
kbusunho <135941352+kbusunho@users.noreply.github.com>
2025-08-25 15:21:53 +0900

    2
    2

---
+++ src/data/footerData.js
@@ -0,0 +1,57 @@
+export const footerCompany ={
+  logo: "img/logo.svg",
+  ceo: "최선호",
+  address: "12051 경기도 남양주시 진접읍 경복대로 425",
+  bizNum: "031-570-9568",
+  salesReport: "Copyright 2024 KYUNGBOK UNIVERSITY. All Rights Reserved.",
+  privacyOfficer: "최선호",
+  tel: "031-570-9901",
+  email: "help@kbu.ac.kr",
+  copyright: "COPYRIGHT© 2025 TOCOBO. ALL RIGHT RESERVED."
+}
+
+export const footerCenterMenu =[
+  {
+    title: "Brand",
+    links: ["Brand Story", "Collection"]
+  },
+  {
+    title: "Shop",
+    links: [
+      "All Product",
+      "Cleansing",
+      "Toner",
+      "Essence",
+      "Creams",
+      "Sun",
+      "Set/Etc"
+    ]
+  },
+  {
+    title: "Promotion",
+    links: ["Promotion"]
+  },
+  {
+    title: "Community",
+    links: ["Notice", "FAQ", "Review", "1:1 CS"]
+  }
+]
+
+
+export const footerInfoLinks=[
+  { text: "이용약관", href: "#" },
+  { text: "개인정보처리방침", href: "#" },
+  { text: "이용안내", href: "#" }
+]
+
+export const snsIcons =[
+  { src: "img/icon_c.svg", alt: "icon" },
+  { src: "img/icon_ins.svg", alt: "icon" },
+  { src: "img/icon_y.svg", alt: "icon" }
+]
+
+export const customerCenter ={
+  tel: "04-1234-5678",
+  time: "AM 09:00 - PM 18:00 (Lunch 12:00 - 13:00)",
+  off: "토, 일요일 및 공휴일 휴무"
+}
